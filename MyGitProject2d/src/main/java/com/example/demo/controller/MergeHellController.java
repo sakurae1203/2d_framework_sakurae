32e1889ca56e4e72bddd81bd1180c71a91aab3c1
sakurae1203 <ibuki.m.3030@gmail.com>
2023-11-30 09:47:19 +0900

    aaaaaaa

---
+++ MyGitProject2d/src/main/java/com/example/demo/controller/MergeHellController.java
@@ -0,0 +1,16 @@
+
+package com.example.demo.controller;
+
+import java.util.Random;
+public class MergeHellController {
+	public static void main(String[] args) {
+		//kadaiA();
+		kadaiB();
+	}
+
+	static void kadaiB() {
+		 Random rand = new Random();
+		    int x = rand.nextInt(100);
+		    System.out.println(x+"が出ました");
+	}
+}
