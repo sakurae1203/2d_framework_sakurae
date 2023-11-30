6d0bdb6cdfc4e38111376f56f0a5432c44089ba3
matuzawa <matu.nao071627@gmail.com>
2023-11-30 09:48:50 +0900

    Aの課題

---
+++ MyGitProject2d/src/main/java/com/example/demo/controller/MergeHellController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+public class MergeHellController {
+	public static void main(String[] args) {
+		kadaiA();
+		kadaiB();
+	}
+
+		static void kadaiA() {
+			for(int a=0;a<=10;a++) {
+				System.out.println("現在"+a+"回目のループです");
+			}
+		}
+
+}
